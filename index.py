d7db35fce1bc0f91945d1f958f87fb9976e4ba46
WilonRamirez <wdrs2000@outlook.com>
2022-05-31 07:56:36 -0500

    Primero

---
+++ index.py
@@ -0,0 +1,24 @@
+#Importacion de flask
+from flask import Flask, render_template
+
+#variable para el main
+app = Flask(__name__, template_folder= "templates")
+
+#funcion para cargar el html/pagina del cliente
+@app.route("/")
+def principal():
+    return render_template("cliente.html")
+
+#funcion para cargar el html/pagina del admin
+@app.route("/admin")
+def principal():
+    return render_template("admin.html")
+
+#funcion para cargar el html/pagina del tienda
+@app.route("/tienda")
+def principal():
+    return render_template("tienda.html")
+
+#Ejecucion del main
+if __name__ == "__main__":
+    app.run(debug=True)
